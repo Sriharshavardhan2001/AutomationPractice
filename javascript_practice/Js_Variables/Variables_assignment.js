4e9a7e28e9d748f52cdbb09d13c31a9353ba473b
Sriharshavardhan2001 <sriharshavardhaneslavath@gmail.com>
2026-08-28 14:28:21 +0530

    Added a file

--- javascript_practice/Js_Variables/Variables_assignment.js
+++ javascript_practice/Js_Variables/Variables_assignment.js
@@ -63,6 +63,55 @@ console.log("Total amount: ", total_amount)
 
 /// Question 5: Compound Interest
 
+/* Principal = ₹50,000
+Rate = 10%
+Time = 2 years
+*/
+
+Principal = 50000
+rate = 10
+time = 2
+
+Amount = (Principal*(1+rate/100)**time)
+CI = Amount-Principal
+
+console.log("value of CI:", CI)
+
+
+/// Question 6: Celsius to Fahrenheit
+
+Celsius = 35
+
+Fahrenheit = (Celsius * 9 / 5) + 32
+
+console.log("value of Celsius:", Fahrenheit)
+
+
+/// Question 7: BMI Calculator
+
+weight = 72
+Height = 1.75
+
+BMI = weight/ Height**2
+
+console.log("value of BMI:", BMI)
+
+
+/// Question 8: Rectangle Area and Perimeter
+
+Length = 15
+Width = 8
+
+var area = Length*Width
+var perimeter = 2 * (Length+Width)
+
+console.log("value of area:", area)
+console.log("value of perimeter:", perimeter)
+
+
+
+
+
 
 
 
